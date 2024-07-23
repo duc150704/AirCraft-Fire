41af48a60acf340e70a0ee9893759fd2d4082ef8
duc150704 <nvduc150704@gmail.com>
2024-07-23 22:53:39 +0700

    fix bug

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] protected Vector3 worldPositon;
-    [SerializeField] protected float speed = 0.1f;
+    [SerializeField] protected float speed = 0.3f;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
     private void FixedUpdate()
     {
         this.worldPositon = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         this.worldPositon.z = 0;
         Vector3 newPos = Vector3.Lerp(transform.position, worldPositon, speed);
         transform.position = newPos;
+
+
     }
 
 }
