c1cb6f2eafdfb2d275afe0f2cfc1a489a6d7872f
duc150704 <nvduc150704@gmail.com>
2024-07-23 21:50:03 +0700

    Them may bay va nhieu asset

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected Vector3 worldPositon;
+    [SerializeField] protected float speed = 0.1f;
+    private void FixedUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        this.worldPositon = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.worldPositon.z = 0;
+        Vector3 newPos = Vector3.Lerp(transform.position, worldPositon, speed);
+        transform.position = newPos;
     }
 
 }
